db6dacb4efb08077d1f209d500a857f215ce09b1
testForIgorDev <igor-devloper>
2023-03-14 17:38:22 -0300

    📱 Adicionado a responsividade

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,23 +5,23 @@ import imgNav from '../assets/img-nav.png'
 export function NavBar() {
 
   return (
-    <NavigationMenu.Root className="relative lg:z-[1] lg:justify-between lg:flex z-40">
+    <NavigationMenu.Root className="relative lg:z-[1] lg:justify-between lg:flex z-40 transition-opacity">
       <img src={logo} alt="" className='mt-[18px] ml-6 rounded-md flex mx-auto' />
       <NavigationMenu.List className="center shadow-blackA7 lg:flex list-none rounded-[6px] p-1 text-white mt-[37px] mr-[151px] lg:gap-16 flex gap-16 my-10 mx-10">
         {/* Nav Inicio */}
         <NavigationMenu.Item>
-          <NavigationMenu.Trigger className="font-head flex items-center justify-between">
+          <NavigationMenu.Trigger className="font-head flex items-center justify-between opacity-50 hover:opacity-100">
             <a href="#home">
               inicio{' '}
             </a>
           </NavigationMenu.Trigger>
         </NavigationMenu.Item>
-        
+        {/* Nav Servico */}
         <NavigationMenu.Item >
-          <NavigationMenu.Trigger className="font-head flex items-center justify-between">
+          <NavigationMenu.Trigger className="font-head flex items-center justify-between opacity-50 hover:opacity-100">
             Serviços{' '}
           </NavigationMenu.Trigger>
-          <NavigationMenu.Content className="overflow-hidden data-[motion=from-start]:animate-enterFromLeft data-[motion=from-end]:animate-enterFromRight data-[motion=to-start]:animate-exitToLeft data-[motion=to-end]:animate-exitToRight absolute flex w-full sm:w-auto bg-[#00040F] z-[1] justify-center">
+          <NavigationMenu.Content className="overflow-hidden data-[motion=from-start]:animate-enterFromLeft data-[motion=from-end]:animate-enterFromRight data-[motion=to-start]:animate-exitToLeft data-[motion=to-end]:animate-exitToRight absolute flex w-full sm:w-screen bg-[#00040F] z-[1] justify-center">
             <ul className="one m-0 grid list-none gap-x-[10px] p-[22px] sm:w-[500px] sm:grid-cols-[0.75fr_1fr]">
               <li className="row-span-3 grid">
                 <NavigationMenu.Link asChild>
@@ -31,7 +31,7 @@ export function NavBar() {
                     href="/"
                   >
 
-                    <img src={imgNav} alt="" className='flex justify-center items-center text-center' />
+                    <img src={imgNav} alt="" className='flex justify-center items-center text-center max-w-[200px]'  />
                   </a>
                 </NavigationMenu.Link>
               </li>
@@ -49,23 +49,40 @@ export function NavBar() {
             </ul>
           </NavigationMenu.Content>
         </NavigationMenu.Item>
-
+        {/* Nav Contato */}
         <NavigationMenu.Item>
-          <NavigationMenu.Trigger className="font-head flex items-center justify-between">
-            inicio{' '}
+          <NavigationMenu.Trigger className="font-head flex items-center justify-between opacity-50 hover:opacity-100">
+            Contato{' '}
           </NavigationMenu.Trigger>
-          <NavigationMenu.Content className="absolute top-0 left-0 w-full sm:w-auto">
-            <ul className="m-0 grid list-none gap-x-[10px] p-[22px] sm:w-[600px] sm:grid-flow-col sm:grid-rows-3">
-            </ul>
-          </NavigationMenu.Content>
         </NavigationMenu.Item>
-
+          {/* Nav Sobre */}
         <NavigationMenu.Item>
-          <NavigationMenu.Trigger className="font-head flex items-center justify-between">
-            inicio{' '}
+          <NavigationMenu.Trigger className="font-head flex items-center justify-between opacity-50 hover:opacity-100">
+            Sobre{' '}
           </NavigationMenu.Trigger>
-          <NavigationMenu.Content className="absolute top-0 left-0 w-full sm:w-auto">
-            <ul className="m-0 grid list-none gap-x-[10px] p-[22px] sm:w-[600px] sm:grid-flow-col sm:grid-rows-3">
+          <NavigationMenu.Content className="overflow-hidden data-[motion=from-start]:animate-enterFromLeft data-[motion=from-end]:animate-enterFromRight data-[motion=to-start]:animate-exitToLeft data-[motion=to-end]:animate-exitToRight absolute flex w-full sm:w-screen bg-[#00040F] z-[1] justify-center">
+            <ul className="one m-0 grid list-none gap-x-[10px] p-[22px] sm:w-[500px] sm:grid-cols-[0.75fr_1fr]">
+              <li className="row-span-3 grid">
+                <NavigationMenu.Link asChild>
+                  <a
+                    className="focus:shadow-violet7 from-purple9 to-indigo9 flex 
+                  h-full w-full select-none flex-col justify-center rounded-[6px] bg-gradient-to-b p-[25px] no-underline outline-none focus:shadow-[0_0_0_2px]"
+                    href="/"
+                  >
+
+                    <img src={logo} alt="" className='flex  items-center text-center rounded-lg ' />
+                  </a>
+                </NavigationMenu.Link>
+              </li>
+              <li>
+                <h1 className="p-4 text-white font-head text-lg text-center">Sobre nós:</h1>
+                <p className='text-white font-main text-center gap-1 text-sm'>
+                  " Focamos em criar uma solução adequada para cada problema de nossos clientes, sem <br />
+                  trazer uma preocupação com preços, conversamos sobre o projeto ou serviço desejado e <br />
+                  fechamos um acordo que ambas partes fique satisfeito. Nosso objetivo está em sempre <br />
+                  entregar uma experiência impecável para quem contrata nossos serviços. "
+                </p>
+              </li>
             </ul>
           </NavigationMenu.Content>
         </NavigationMenu.Item>
